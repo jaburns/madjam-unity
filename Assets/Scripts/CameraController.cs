7ad08aa986797008f6652195085eeb2dcf50bec3
Jeremy Burns <j@jaburns.net>
2015-07-19 12:22:21 -0600

    Updated stuff

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,24 @@ public class CameraController : MonoBehaviour
 {
     public Transform Target;
 
-    // Update is called once per frame
-    void LateUpdate ()
+    Rigidbody2D _rb;
+
+    void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    //void LateUpdate ()
+    //{
+    //    var newPosition = Target.transform.position.WithZ(transform.position.z);
+    //    transform.position += (newPosition - transform.position) / 10;
+    //}
+
+    void FixedUpdate()
+    {
+        // SmoothDamp
         var newPosition = Target.transform.position.WithZ(transform.position.z);
-        transform.position += (newPosition - transform.position) / 10;
+        var targPos = _rb.position + (newPosition.AsVector2() - _rb.position) / 10;
+        _rb.MovePosition(targPos);
     }
 }
---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Ground : MonoBehaviour
+{
+    public List<Vector2> Nodes;
+    public float JaggySize = 0.5f;
+    public float StepSize = 1.5f;
+    public bool FlipNormals;
+}
