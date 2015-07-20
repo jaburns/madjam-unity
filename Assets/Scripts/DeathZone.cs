09a77d4b932f5eccb12f442ce65bc0dbeb145d37
Jeremy Burns <j@jaburns.net>
2015-07-20 17:58:53 -0600

    Added simple deathzone trigger script which reloads level

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "BlobTrigger") {
+            Application.LoadLevel(0);
+        }
+    }
+}
