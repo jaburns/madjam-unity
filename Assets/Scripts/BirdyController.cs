cc03c299911b4ea0ce0e04ec2cb989fbb84df735
Jeremy Burns <j@jaburns.net>
2015-07-22 18:04:25 -0600

    Removed old crappy bird controller

--- Assets/Scripts/BirdyController.cs
+++ Assets/Scripts/BirdyController.cs
@@ -14,7 +14,37 @@ public class BirdyController : MonoBehaviour
 	void FixedUpdate()
     {
         if (_blobBinder.HasBlob) {
-            _rb.AddForce(Vector2.right * 10);
+            if (Controls.Instance.Act == Controls.ControlState.Press) {
+                _rb.isKinematic = false;
+                _rb.AddForce(Vector2.up * 100);
+            }
+            if (!_rb.isKinematic) {
+                if (Controls.IsDown(Controls.Instance.Right)) {
+                    _rb.AddForce(Vector2.right * 10);
+                } else if (Controls.IsDown(Controls.Instance.Left)) {
+                    _rb.AddForce(-Vector2.right * 10);
+                }
+            }
         }
     }
+
+    static bool normalIsFloor(Vector2 normal)
+    {
+        return normal.y > 0.5;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        foreach (var contact in col.contacts) {
+            if (normalIsFloor(contact.normal)) {
+                attachToFloor();
+            }
+        }
+    }
+
+    void attachToFloor()
+    {
+        _rb.isKinematic = true;
+    }
 }
+
