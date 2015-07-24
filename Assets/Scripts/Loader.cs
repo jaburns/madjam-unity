aee41da34e121cf50628a47dcc9ac8896da9d082
Jeremy Burns <j@jaburns.net>
2015-07-24 15:02:44 -0600

    Added goo color select

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -2,13 +2,40 @@
 
 public class Loader : MonoBehaviour
 {
+    public Material GooMaterial;
+    public Color[] GooColors;
+
+    int _colorIndex;
+
     void Start ()
     {
         BlobController.s_checkedPoint = null;
+        GooMaterial.color = GooColors[_colorIndex];
     }
 
     public void OnClickStart()
     {
         Application.LoadLevel("Level");
+    }
+
+    public void OnRightArrow()
+    {
+        _colorIndex++;
+        if (_colorIndex >= GooColors.Length) _colorIndex = 0;
+        GooMaterial.color = GooColors[_colorIndex];
+    }
+
+    public void OnLeftArrow()
+    {
+        _colorIndex--;
+        if (_colorIndex < 0) _colorIndex = GooColors.Length - 1;
+        GooMaterial.color = GooColors[_colorIndex];
+    }
+
+    public void OnUpArrow()
+    {
+    }
+    public void OnDownArrow()
+    {
     }
 }
