308f3d7b7085b6b6ee5bf6e9aa82f499634b2b8d
Jeremy Burns <j@jaburns.net>
2015-07-23 21:24:21 -0600

    Implemented fade between music tracks

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -8,6 +8,12 @@ public class MusicController : MonoBehaviour
 
     public AudioSource[] Musics;
 
+    int _prevIndex = -1;
+    int _thisIndex = -1;
+
+    float _t;
+    bool _fading;
+
     void Awake()
     {
         reset();
@@ -22,7 +28,26 @@ public class MusicController : MonoBehaviour
 
     public void SetMusic(int index)
     {
-        reset();
-        Musics[index].volume = 1;
+        _fading = true;
+        _t = 0;
+        _thisIndex = index;
+    }
+
+    void FixedUpdate()
+    {
+        if (!_fading) return;
+
+        _t += 0.03f;
+        if (_t >= 1) {
+            _t = 1;
+            _fading = false;
+        }
+
+        if (_thisIndex >= 0) Musics[_thisIndex].volume = _t;
+        if (_prevIndex >= 0) Musics[_prevIndex].volume = 1 - _t;
+
+        if (!_fading) {
+            _prevIndex = _thisIndex;
+        }
     }
 }
